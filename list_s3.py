ff27b1db400bda6aed0813f4d9c5e375c63f5f33
Jaroslav Hranička <me@hranicka.cz>
2018-04-20 10:41:06 +0200

    Simplify functionality

--- list_s3.py
+++ list_s3.py
@@ -1,143 +1,66 @@
 #!/usr/bin/python
-# marek kuczynski
-# @marekq
-# www.marek.rocks
 
-# configure the bucketname to write result CSV file to
-bucketn	= 'lists3marek'	
-
-# AWS bucket API key and bucket name to write the CSV file with results to - if you leave these blank, the lambda service role will be used which is prefered
-s3acck = ''
-s3seck = ''
-s3_region_name = 'eu-west-1'
-
-# AWS account to list the S3 buckets and files in them for - if you leave these blank, the lambda service role will be used which is prefered
-aws_acck = ''
-aws_seck = ''
-aws_region_name = 'eu-west-1'
+import boto3
+import time
+import config
 
 
-########## ! # ! # don't touch anything below this line # ! # ! ##########
+# execute
+def main():
+    get_s3(config.aws_acck, config.aws_seck, config.aws_region_name, config.s3_bname)
 
-import boto3, time, re, datetime, json, os, sys
 
 # retrieve metadata about all buckets
-def get_s3(aws_acck, aws_seck, aws_sest, aws_region_name):
-	global resu
-	resu = []
-	
-	session = boto3.session.Session()
+def get_s3(aws_acck, aws_seck, aws_region_name, s3_bname):
+    global resu
+    resu = []
 
-	if len(aws_seck) == 0 and len(aws_acck) == 0:
-		sts_client = session.client('s3', region_name = aws_region_name)
-		
-	else:
-		sts_client = session.client('s3', region_name = aws_region_name, aws_access_key_id = aws_acck, aws_secret_access_key = aws_seck, aws_session_token = '')
-		
-		f = sts_client.list_buckets()
-		
-		if f != '':
-			for y in f['Buckets']:
-				try:
-					r    	= sts_client.get_bucket_acl(Bucket = y['Name'])
-					owne 	= r['Owner']['DisplayName']
-					gran 	= r['Grants'][0]['Permission']
-					
-				except Exception as e:
-					owne	= ''
-					grant	= ''
-					print e
+    session = boto3.session.Session()
 
-					name	= y['Name']
-					crea	= y['CreationDate']
-					loca	= sts_client.get_bucket_location(Bucket = name)['LocationConstraint']
-					
-					get_s3_obj(sts_client, name, crea, owne, gran, '')
-					# get_s3_web(sts_client, name)
+    sts_client = session.client(
+        's3',
+        region_name=aws_region_name,
+        aws_access_key_id=aws_acck,
+        aws_secret_access_key=aws_seck,
+        aws_session_token=''
+    )
+
+    get_s3_obj(sts_client, s3_bname, '')
 
 
 # get object metadata from the bucket
-def get_s3_obj(sts_client, name, crea, owne, gran, tok):
-	if len(tok) > 0:
-		b = sts_client.list_objects_v2(Bucket = name, FetchOwner = True, MaxKeys = 1000, ContinuationToken = tok)
-	else:
-		b = sts_client.list_objects_v2(Bucket = name, FetchOwner = True, MaxKeys = 1000)
+def get_s3_obj(sts_client, name, tok):
+    if len(tok) > 0:
+        b = sts_client.list_objects_v2(Bucket=name, FetchOwner=True, MaxKeys=1000, ContinuationToken=tok)
+    else:
+        b = sts_client.list_objects_v2(Bucket=name, FetchOwner=True, MaxKeys=1000)
+
+    # capture metadata about objects in bucket
+    if b.has_key('Contents'):
+        timest = '%Y-%m-%d %H:%M:%S'
+
+        for z in b['Contents']:
+            size = str(z['Size'])
+            skey = str(z['Key'])
+            stor = z['StorageClass']
+            fmod = z['LastModified']
+
+            funix = int(time.mktime(fmod.timetuple()))
+            fitim = fmod.strftime(timest)
+
+            # write the results to a list
+            x = [name, skey, size, stor, fitim, funix]
+            y = ','.join(map(str, x))
+
+            resu.append(y)
+            print(y)
+
+    # if there is a token, retrieve additional files by calling the script again
+    if b.has_key('NextContinuationToken'):
+        tok = b['NextContinuationToken']
+        get_s3_obj(sts_client, name, tok)
 
 
-	# capture metadata about objects in bucket
-	if b.has_key('Contents'):
-		timest = '%Y-%m-%d_%H-%M'
-
-		for z in b['Contents']:
-			size	= str(z['Size'])
-			skey	= str(z['Key'])
-			stor	= z['StorageClass']
-			fmod	= z['LastModified']
-
-			bunix	= int(time.mktime(crea.timetuple()))
-			funix	= int(time.mktime(fmod.timetuple()))
-			butim	= crea.strftime(timest)
-			fitim	= fmod.strftime(timest)
-			reg     = ''
-			
-			# write the results to a list
-			x       = [name, skey, size, reg, owne, gran, stor, butim, bunix, fitim, funix]
-			y   	= ''
-			
-			for xx in x:
-				y += str(xx)+','
-				
-				resu.append(y[:-1])
-				print 'RUN: '+y[:-1]
-
-
-	# if there is a token, retrieve additional files by calling the script again
-	if b.has_key('NextContinuationToken'):
-		tok	= b['NextContinuationToken']
-		get_s3_obj(sts_client, name, crea, owne, gran, tok)
-
-
-# get s3 website attached to a bucket
-def get_s3_web(sts_client, bucket):
-	try:
-		s3web = sts_client.get_bucket_website(Bucket = bucket)
-		print str(s3web)
-
-	except Exception as e:
-		print e
-
-
-# write the results to result.csv and timestamped file in directory s3log
-def file_write():
-	s3tag	= 'bucket,object-name,object-size,bucket-region,bucket-owner,'
-	s3tag   += 'bucket-permission,storage-type,bucket-create-date,'
-	s3tag   += 'bucket-create-unix,file-create-date,file-create-unix'
-	
-	now     = datetime.datetime.now()
-	timest  = now.strftime()
-	filen   = 'lambda-'+str(timest)+'.csv'  
-	
-	f1	    = open('/tmp/lambda-'+str(timest)+'.csv', 'w')
-	f1.write(s3tag+'\n')
-	for x in resu:
-		f1.write(x+'\n')
-		f1.close
-		
-	put_s3(bucketn, filen)
-
-
-def put_s3(bucketn, filen):
-	session     = boto3.session.Session()
-	if len(s3acck) == 0 and len(s3seck) == 0:
-		sts_client  = session.client('s3', region_name = s3_region_name)    
-	else:
-		sts_client  = session.client('s3', region_name = s3_region_name, aws_access_key_id = s3acck, aws_secret_access_key = s3seck)
-			
-	sts_client.upload_file('/tmp/'+filen, bucketn, filen)
-				
-				
-# kick off the lambda function
-
-def lambda_handler(event, context):
-	get_s3(aws_acck, aws_seck, '', aws_region_name)
-	file_write()
+# execute
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -1,94 +1,35 @@
-list_s3
-=========
+# list-s3
 
-List all file objects stored in S3 buckets attached to an AWS account using AWS API keys. 
-
-
-Description
-------------
-
-The list_s3 tool can be used to create timestamped CSV reports about files stored within S3 buckets of an AWS account. This is helpful if you are a user of multiple S3 buckets with nested folders which becomes a bit difficult using just the AWS console view. Using the reports it becomes easy to review files stored in buckets, including information about the owner, access permissions and modification times. 
-
-Example output is shown below;
-
-| bucket       | object-name            | object-size  | bucket-region  | bucket-owner  | bucket-permission  | storage-type  | bucket-create-date  | bucket-create-unix  | file-create-date  | file-create-unix | 
-|--------------|------------------------|--------------|----------------|---------------|--------------------|---------------|---------------------|---------------------|-------------------|------------------|
-| marek.rocks  | index.html             | 211613       | eu-west-1      | marekq        | FULL_CONTROL       | STANDARD      | 2016-06-05_03-00    | 1465092040          | 2016-06-03_21-36  | 1464986192       | 
-| marek.rocks  | papers.html            | 899          | eu-west-1      | marekq        | FULL_CONTROL       | STANDARD      | 2016-06-05_03-00    | 1465092040          | 2016-06-03_21-36  | 1464986192       | 
-| marek.rocks  | papers/                | 0            | eu-west-1      | marekq        | FULL_CONTROL       | STANDARD      | 2016-06-05_03-00    | 1465092040          | 2016-06-03_21-36  | 1464986206       |
-| marek.rocks  | papers/lia.pdf         | 1472885      | eu-west-1      | marekq        | FULL_CONTROL       | STANDARD      | 2016-06-05_03-00    | 1465092040          | 2016-06-03_21-36  | 1464986206       |
-| marek.rocks  | papers/ot.pdf          | 476803       | eu-west-1      | marekq        | FULL_CONTROL       | STANDARD      | 2016-06-05_03-00    | 1465092040          | 2016-06-03_21-36  | 1464986207       |
-| ...          | ...                    | ...          | ...            | ...           | ...                | ...           | ...                 | ...                 | ...               | ...              |
+Lists all file objects stored in S3 bucket, including their sizes.
 
 
-Installation
-------------
+## Requirements
 
-The tool has been tested on Debian, but should work on other platforms too. Besides Python, install the following package using 'python-pip';
+* python 3.6+
+* pip
+* boto3 package
 
-    $ pip install boto3 
+## Installation
 
-Next, edit the 'creds.py' file to include your AWS API credentials as follows;
+    cp config.py.dist config.py
 
-    aws_access_key_id           = 'accesskeygoeshere'
-    aws_secret_access_key       = 'secretkeygoeshere'
-    region_name                 = 'regiongoeshere'
-
-You can run 'python list_s3.py' to check if the tool works well on your machine and if valid credentials were entered. As an alternative, you can also submit credentials as arguments which can be useful if you want a one time report;
-
-        $ python list_s3.py <aws_access_key> <aws_secret_access_key> <region_name>
+Edit `config.py` and add your AWS credentials and S3 Bucket name.
 
 
-Usage
------
+## Usage
 
-The tool picks up credentials configured in 'creds.py' in the folders root. 
+Run the script. 
 
-    $ python list_s3.py
+    ./list_s3.py
 
-Results of the scan are shown on screen and stored in "results.csv" in the current directory, in addition a timestamped copy of the results file is stored in folder 'results' in the current directory.
-
-
-AWS IAM permissions
--------------------
-
-It is recommended to create a readonly AWS IAM account specifically for usage with this tool using a minimal amount of priviledges - you don't want someone to read the actual file contents or delete all files if the keys ever get stolen. Never use your root credentials for these kind of reports, always use specific roles for them. 
-
-I recommend to use the following policy document;
-        
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "s3:GetBucketAcl",
-                    "s3:GetBucketLocation",
-                    "s3:GetBucketPolicy",
-                    "s3:GetBucketWebsite",
-                    "s3:ListAllMyBuckets",
-                    "s3:GetObjectAcl",
-                    "s3:ListBucket",
-                    "s3:ListObjects"
-                ],
-                "Resource": [
-                    "arn:aws:s3:::*"
-                ]
-            }
-        ]
-    }
-        
-
-Future work
------------
-
-~~- Convert the code to be AWS Lambda compatible~~
-~~- Integrate easy way to push the results to an S3 bucket~~
-- Make the code work with multiple AWS API keys defined
-- Enable STS support in case multiple accounts are used
+Results of the scan are shown in stdout.
 
 
-Contact
--------
+## Security
 
-For any questions or fixes, please reach out to @marekq! 
+It is recommended to create a readonly AWS IAM account specifically for usage with this tool using a minimal amount of privileges.
+
+
+## Credits
+
+This tool is a fork of https://github.com/marekq/list-s3
